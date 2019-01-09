11cf19e8b57f7faba6f4fde0de8c8e09be3ef0e2
siravich-khongrod <40408027+siravich-khongrod@users.noreply.github.com>
2019-01-09 13:59:16 -0600

    Update createIndex.py

--- createIndex.py
+++ createIndex.py
@@ -13,3 +13,27 @@ for i in range(0,len(docs_token)):
 
 for key in index.keys():
 	print(key+": "+str(index[key]))
+
+	
+	
+	
+##### SEARCH #####
+# index AND query
+print(search_and(index,'index','query'))
+print(search_and(index,'index','queri'))
+
+# index OR query
+print(search_or(index,'index','queri'))
+
+# index AND (NOT query)
+# do you mean queri?
+# index AND (NOT queri) <- stemmed query
+print(set(index['index']) - set(index['queri']))
+# index AND (NOT query)
+print(set(index['index']) - set(index['query']))
+
+# (search AND query) OR (search AND retrieve)
+search_and(index,'search','query') | search_and(index,'search','retrieve')
+
+# (index OR cluster) AND (web OR system)
+search_or(index,'index','cluser') & search_or(index,'web','system')
