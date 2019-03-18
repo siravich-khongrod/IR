c07dd54e5f1091ed6504e8528e31b4329cc6c741
csc575 final project <40408027+siravich-khongrod@users.noreply.github.com>
2019-03-17 22:34:54 -0500

    Add files via upload

--- resume_submit/resumeform.php
+++ resume_submit/resumeform.php
@@ -1,10 +1,14 @@
-<html>
-<head>
-<link rel='stylesheet' href='simple-job-board-public.css' type='text/css' media='all' />
-<link rel='stylesheet' href='grid.css' type='text/css' media='all' />
-<link rel='stylesheet' href='custom.css' type='text/css' media='all' />
+<html><head>
+	<style>
+	img{
 
-<?php $target_file_name = md5(uniqid(rand(), true)); ?>
+	float: left;
+	}
+</style>
+<link rel="stylesheet" href="simple-job-board.css" type="text/css" media="all">
+<link rel="stylesheet" href="grid.css" type="text/css" media="all">
+<link rel="stylesheet" href="custom.css" type="text/css" media="all">
+
   	<script language="javascript" type="text/javascript">
 	//<--
 	function startUpload(){
@@ -34,6 +38,10 @@
 </head>
 <body class="jobpost-template-default single single-jobpost postid-216  color-custom style-default layout-full-width mobile-tb-left hide-love header-classic sticky-header sticky-white ab-hide subheader-title-left menu-line-below menuo-right betheme sjb">
 
+<div style="margin-top:50px;margin-bottom: 0px; text-align:center;height:100px">
+		<img src="home_title.png" id="home_title_image" alt="title">
+	
+			</div>	
 <div class="container sjb-container" id="container"><div id="content" class="sjb-content" role="main">
 
 
@@ -45,7 +53,7 @@
 
 <div class="row">
     <div class="job-detail">        
-        <h3><span class="job-title" style="color: #d84946;    margin: 0;
+        <h3><span class="job-title" style="color: 	#FF4500;    margin: 0;
     padding: 0;
     border: 0;
     font-size: 100%;
@@ -107,9 +115,9 @@
 							<!--div class="file"><span>No file chosen</span>
 								<div>Browse</div>
                                                                 
-								<input type="file" name="applicant_resume" id="applicant_resume" value="<?php echo $target_file_name; ?>" class="sjb-attachment form-control " required="required" size="50" style="opacity: 0;">
+								<input type="file" name="applicant_resume" id="applicant_resume" value="736dd0cfc9e1a650040b17fdc417c71e" class="sjb-attachment form-control " required="required" size="50" style="opacity: 0;">
 							</div><span class="sjb-invalid-attachment validity-note" id="file-error-message"></span></div-->
-                                                        <!--div> <input id="applicant_resume" name="applicant_resume" type="text" value="<?php echo $target_file_name; ?>" > </div-->
+                                                        <!--div> <input id="applicant_resume" name="applicant_resume" type="text" value="736dd0cfc9e1a650040b17fdc417c71e" > </div-->
 						
 						<div class="form-group">
 							<label for="applicant_resume">Would you like to submit resume to recruitement firm?</label>
@@ -118,25 +126,25 @@
 							<input type="radio" name="recruit" value="0"> No<br>
 						</div>
 						
-						<div> <input type="hidden" id="vhphoto" name="vhphoto" type="text" value="<?php echo $target_file_name; ?>" > </div>
+						<div> <input type="hidden" id="vhphoto" name="vhphoto" value="736dd0cfc9e1a650040b17fdc417c71e"> </div>
 						<div id="">
-							<label>Select File</label><br/>
-							<input type="file" name="file" id="file" class="sjb-attachment form-control valid" required />
-							<input type="hidden" id="filename" name="filename" type="text" value="<?php echo $target_file_name; ?>">
+							<label>Select File</label><br>
+							<input type="file" name="file" id="file" class="sjb-attachment form-control valid" required="">
+							<input type="hidden" id="filename" name="filename" value="736dd0cfc9e1a650040b17fdc417c71e">
 							<!--input type="submit" value="Upload" class="submit" /-->
 						</div>
 						
 						
 						<div class="form-group" id="sjb-form-padding-button">
-							<button class="btn btn-primary app-submit">Submit</button>
+							<button class="btn btn-primary app-submit" >Submit</button>
 							<!--input type="submit" id=""/-->
 						</div>
 					</div>    
 				</div>
-			</form>
+			
 
-		</div>
+		</div></form>
 	</div>
 </div>
-</body>
-</html>
+
+</div></div></div></body></html>
